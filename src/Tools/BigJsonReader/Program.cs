d70311099eda1b602bce30bd156c9eef4bd549af
pacuitinfo <pacuit.info@gmail.com>
2025-10-30 00:12:41 +0800

    asf

--- src/Tools/BigJsonReader/Program.cs
+++ src/Tools/BigJsonReader/Program.cs
@@ -1742,7 +1742,7 @@ var fileContext2 =GenerateCashierReportePdf(servicesReports, dateStart, dateEnd)
 var fileContext3 =GenerateExcel(servicesReports);
 var fileContext4 =GenerateCashierReportExcel(servicesReports, dateStart, dateEnd);
 
-if (fileContext1 != null || fileContext2 != null || fileContext3 != null || fileContext4 != null)
+if (true)
 {
     var fileSoaReportName = $"cashier-pdf-reports/cache/{soaRegionKey}"; 
     var uploadResultSoaReport = await GitHubHelper.UploadStream(
